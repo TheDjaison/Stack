63264a0cd230443b25c820f097e1caa47ff97efa
HENRIQUE BRENDON <henriquebrendon26@gmail.com>
2022-12-14 21:59:39 -0300

    Menu do programa

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,45 @@ public class Main {
 	public static void main(String[] args) {
 		// Aprendendo Estrutura de dados!!
 		// Passo 1: importando blibliotecas
+		// Passo 2: Criar Menu do programa
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		
+		int op;
+		do {
+			showMenu();
+			op = sc.nextInt();
+			switch(op) {
+			case 1: {
+				System.out.println("Insira elemento na lista:");
+				break;
+			}
+			case 2: {
+					System.out.println("Lista: ");
+				break;
+			}		
+			case 3: {
+				System.out.println("Fim do programa");
+				break;
+			}
+			default:
+				System.out.println("Erro na execução do programa!");
+			}
+
+		}while(op !=3);
+		
+		
+		
+		
+		
 		sc.close();
 
+	}
+	public static void showMenu() {
+		System.out.println("1 - Inserir elemento na lista ");
+		System.out.println("2 - Mostrar lista ");
+		System.out.println("3 - Sair ");
 	}
 
 }
